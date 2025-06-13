8f3f08296dbad2a74bf3070eb2cb4275562f3727
clrn1w <nikitamet2004@yandex.ru>
2025-06-13 15:43:34 +0300

    update sidebar

--- constants/sidebarList.ts
+++ constants/sidebarList.ts
@@ -1,7 +1,12 @@
+import AutoIcon from '@/components/icons/AutoIcon'
 import BeautyIcon from '@/components/icons/BeautyIcon'
+import ClothIcon from '@/components/icons/ClothIcon'
+import CouponIcon from '@/components/icons/CouponIcon'
 import EducationIcon from '@/components/icons/EducationIcon'
+import ElectronicIcon from '@/components/icons/ElectronicIcon'
 import FireIcon from '@/components/icons/FireIcon'
 import HiddenIcon from '@/components/icons/HiddenIcon'
+import JewelryIcon from '@/components/icons/JewelryIcon'
 import MarkIcon from '@/components/icons/MarkIcon'
 import StarsIcon from '@/components/icons/StarsIcon'
 
@@ -20,6 +25,31 @@ export const defaultSidebar = [
 		title: 'Красота и здоровье',
 		Icon: BeautyIcon,
 		href: '/category/beauty',
+	},
+	{
+		title: 'Одежда и обувь',
+		Icon: ClothIcon,
+		href: '/category/cloth',
+	},
+	{
+		title: 'Электроника',
+		Icon: ElectronicIcon,
+		href: '/category/electronic',
+	},
+	{
+		title: 'Для авто',
+		Icon: AutoIcon,
+		href: '/category/auto',
+	},
+	{
+		title: 'Украшения',
+		Icon: JewelryIcon,
+		href: '/category/jewelry',
+	},
+	{
+		title: 'Подарочные сертификаты',
+		Icon: CouponIcon,
+		href: '/category/coupon',
 	},
 ]
 
